bd283785932c98b66f5225ef79f23c76b2549398
spleban <spleban@orangesoftware.com>
2020-04-18 14:32:21 -0700

    add Travis

--- models/Schedule/scheduletable.sql
+++ models/Schedule/scheduletable.sql
@@ -9,7 +9,7 @@ CREATE TABLE provider (
   name VARCHAR(60) NOT NULL,
   email VARCHAR(60) NOT NULL,
   password VARCHAR(20) NOT NULL,
-  product_id INT,
+  service_id INT,
   daily_slots INT,
   PRIMARY KEY (id)
 );
@@ -26,13 +26,13 @@ CREATE TABLE session (
   id INT AUTO_INCREMENT NOT NULL,
   customer_id INT NOT NULL,
   provider_id INT NOT NULL,
-  product_id INT NOT NULL,
+  service_id INT NOT NULL,
   date DATE NOT NULL,
   slot INT NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE product (
+CREATE TABLE service (
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(50) NOT NULL,
   PRIMARY KEY (id)
